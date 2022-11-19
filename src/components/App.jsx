79c462ee61560013efc1ffbc5cf45397fd03812e
Инна <anni31717213@gmail.com>
2022-11-19 19:49:26 +0200

    end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,25 @@ class App extends Component {
     filter: '',
   };
 
+ 	  componentDidMount() {
+	    const contacts = localStorage.getItem('contacts');
+	    const parsedContacts = JSON.parse(contacts);
+	
+	    if (parsedContacts) {
+	      this.setState({
+	        contacts: parsedContacts,
+	        filter: '',
+	      });
+	    }
+	  }
+	
+	  componentDidUpdate( prevState) {
+	    if (this.state.contacts !== prevState.contacts) {
+	      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+	    }
+	  }
+
+  
   addContact = ({ name, number }) => {
     const contact = {
       id: uuidv4(),
